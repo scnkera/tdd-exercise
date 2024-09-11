2e117905deb43bc08df0af4a41a98948563709b1
Christelle Nkera <scnkera@gmail.com>
2024-09-11 18:02:11 -0400

    Updated python main and pytest file

--- test_blackjack_score.py
+++ test_blackjack_score.py
@@ -5,34 +5,63 @@ import pytest
 def test_score_for_pair_of_number_cards():
   # Arrange
   hand = [3, 4]
+  expected_result = 7
 
   # Act
   score = blackjack_score(hand)
 
   # Assert <-- Write assert statement here
+  assert score == expected_result
   
 
-@pytest.mark.skip(reason="no way of currently testing this")
+# @pytest.mark.skip(reason="no way of currently testing this")
 def test_facecards_have_values_calculated_correctly():
-  pass
+  hand = ["Jack", "Queen"]
+  expected_result = 20
 
-@pytest.mark.skip(reason="no way of currently testing this")
+  score = blackjack_score(hand)
+
+  assert score == expected_result
+
+  
+
+# @pytest.mark.skip(reason="no way of currently testing this")
 def test_calculates_aces_as_11_where_it_does_not_go_over_21():
-  pass
+    hand = [2, 6, "Ace"]
+    expected_result = 19
+
+    score = blackjack_score(hand)
+
+    assert score == expected_result
 
 
-@pytest.mark.skip(reason="no way of currently testing this")
+# @pytest.mark.skip(reason="no way of currently testing this")
 def test_calculates_aces_as_1_where_11_would_bust():
-  pass
+    hand = [2, 7, "Ace"]
+    expected_result = 20
 
-@pytest.mark.skip(reason="no way of currently testing this")
+    score = blackjack_score(hand)
+
+    assert score == expected_result
+
+# @pytest.mark.skip(reason="no way of currently testing this")
 def test_returns_invalid_for_invalid_cards():
-  pass
+  hand = [14]
+  expected_result = None
+
+  score = blackjack_score(hand)
+
+  assert score == expected_result
 
 
-@pytest.mark.skip(reason="no way of currently testing this")
+# @pytest.mark.skip(reason="no way of currently testing this")
 def test_returns_invalid_for_list_length_greater_than_5():
-  pass
+  hand = [2, 3, 4, 5, 6, 7]
+  expected_result = None
+
+  score = blackjack_score(hand)
+
+  assert score == None
 
 @pytest.mark.skip(reason="no way of currently testing this")
 def test_returns_bust_for_scores_over_21():
